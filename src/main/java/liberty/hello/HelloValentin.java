ba178841d4fc40fb099c41e68e6e155eb75e2f06
N0210798 <N0210798@LIBP35P-5NSC13Q.lm.lmig.com>
2021-11-16 13:33:57 +0100

    Adding Valentin page.

---
+++ src/main/java/liberty/hello/HelloValentin.java
@@ -0,0 +1,13 @@
+package liberty.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloValentin {
+
+	@GetMapping("/valentin")
+	public String hello() {
+		return "Hello Valentin";
+	}
+}
